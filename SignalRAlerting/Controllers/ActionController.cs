0bd6ebc42d667060ef954ee0ee5a6778ae104d01
Frederik Gheysels <frederik.gheysels@codit.eu>
2019-05-28 13:48:01 +0200

    Install SignalR package

---
+++ SignalRAlerting/Controllers/ActionController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRAlerting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActionController : ControllerBase
+    {
+        // GET api/values
+        [HttpPost("launch")]
+        public ActionResult<IEnumerable<string>> Launch(string action)
+        {
+            return Accepted();
+        }
+
+        // GET api/values/5
+        [HttpPost("respond")]
+        public ActionResult<string> Get(string response)
+        {
+            return Ok();
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
