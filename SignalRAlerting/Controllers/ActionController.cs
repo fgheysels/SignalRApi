6b38cc74774cbfc2daa83a1a769cabfc1d888565
Frederik Gheysels <frederik.gheysels@codit.eu>
2019-05-28 14:01:57 +0200

    Add ActionHub implementation

--- SignalRAlerting/Controllers/ActionController.cs
+++ SignalRAlerting/Controllers/ActionController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRAlerting.Hubs;
 
 namespace SignalRAlerting.Controllers
 {
@@ -10,11 +12,24 @@ namespace SignalRAlerting.Controllers
     [ApiController]
     public class ActionController : ControllerBase
     {
+        private readonly IHubContext<ActionHub> _signalRHub;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActionController"/> class.
+        /// </summary>
+        public ActionController(IHubContext<ActionHub> hub)
+        {
+            _signalRHub = hub;
+        }
+
         // GET api/values
         [HttpPost("launch")]
         public ActionResult<IEnumerable<string>> Launch(string action)
         {
-            return Accepted();
+            return Accepted(new
+            {
+                //ConnectionId = HttpContext.
+            });
         }
 
         // GET api/values/5
---
+++ SignalRAlerting/Hubs/ActionHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRAlerting.Hubs
+{
+    public interface IActionHub
+    {
+        Task Alert(string message);
+    }
+
+    public class ActionHub : Hub<IActionHub>
+    {
+        public async Task Alert(string connectionId, string message)
+        {
+            await Clients.Client(connectionId).Alert(message);
+        }
+
+        public async Task BroadcastAlert(string message)
+        {
+            await Clients.All.Alert(message);
+        }
+    }
+}
